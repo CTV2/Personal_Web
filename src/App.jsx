9f08c7e818c18873db14a3a11cb1e4a883b78f3a
rodneybrown <firewall.brown67@gmail.com>
2025-12-16 18:36:16 -0500

    Initial commit with name

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import xer from '/716.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="Landing Page">
+          <h1>Rodney Brown</h1>
+          <p>Hi, my name is Rodney Brown and I'm a computer
+              science major <br /> I've learned a lot so far but I am excited to enter the
+              industry in a more professional capacity and expand my skills"
+          </p>
+      </div>
+        <img src={xer} alt="xer" />
+
+
+    </>
+  )
+}
+
+export default App
