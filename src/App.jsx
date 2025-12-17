d22a651192791923cb633a067c79dd41f558227b
rodneybrown <firewall.brown67@gmail.com>
2025-12-16 19:53:40 -0500

    removed default text and made landing page

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,18 @@ import './App.css'
 function App() {
 
   return (
-    <>
-      <div className="Landing Page">
-          <h1>Rodney Brown</h1>
-          <p>Hi, my name is Rodney Brown and I'm a computer
-              science major <br /> I've learned a lot so far but I am excited to enter the
-              industry in a more professional capacity and expand my skills"
-          </p>
-      </div>
-        <img src={xer} alt="xer" />
+   <div className="min-h-screen bg-black text-white font-bold flex flex-col justify-center items-center px-6">
+       <h1 className={"text-7xl"}>Rodney Brown</h1>
+
+       <p className={"text-2xl text-green-500 mt-12 text-center"}>
+           Hello my name is Rodney Brown, I'm a computer science student at gvsu<br/>
+           I'm currently looking to use my skills in more practical ways to learn<br/>
+           and expand my experience by building real software.
+       </p>
+       <img src={xer} alt="Rodney Brown"/>
 
 
-    </>
+   </div>
   )
 }
 
