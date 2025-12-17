e89f15ca6772f49ee09738ca35133df32901e1ae
rodneybrown <firewall.brown67@gmail.com>
2025-12-17 12:57:44 -0500

    Added rough react routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {BrowserRouter, createBrowserRouter, RouterProvider} from "react-router";
 import './index.css'
 import App from './App.jsx'
 import AboutMe from "./AboutMe.jsx";
 import Projects from "./Projects.jsx"
 
+const router = createBrowserRouter([
+    {path: '/', element: <App />},
+    {path: '/about', element: <AboutMe />},
+    {path: '/projects', element: <Projects />},
+])
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+      <RouterProvider router = {router}/>
   </StrictMode>,
 )
 
