13a4d680d8bbbf774c2850c0c1b75bf4c4f5b8ab
rodneybrown <firewall.brown67@gmail.com>
2025-12-22 11:24:30 -0500

    Initial Firebase Setup

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -6,6 +6,29 @@ import {
 } from "@/components/ui/dropdown-menu.tsx";
 import {useNavigate} from "react-router-dom";
 
+
+//Firebase
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyC7AN6mnlIDiUdIHB6mXaN4WWtrgusV04Q",
+    authDomain: "personal-website-28c3a.firebaseapp.com",
+    projectId: "personal-website-28c3a",
+    storageBucket: "personal-website-28c3a.firebasestorage.app",
+    messagingSenderId: "915611759396",
+    appId: "1:915611759396:web:9660e6d81b554b6d34b9b1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
 function Projects(){
     const navigate = useNavigate();
     return (
