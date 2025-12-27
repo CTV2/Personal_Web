668b4a4a69a0566daa87c1a71cb00f98329bcfdc
rodneybrown <firewall.brown67@gmail.com>
2025-12-27 18:54:27 -0500

    Rough Admin log print

--- src/Admin_log.jsx
+++ src/Admin_log.jsx
@@ -1,19 +1,32 @@
-import {useNavigate} from "react-router-dom";
+import { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "./firebase";
 import {
     DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
-    DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-
-function log() {
+    DropdownMenuContent, DropdownMenuItem,
+    DropdownMenuLabel, DropdownMenuSeparator,
+    DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu.tsx";
+import {useNavigate} from "react-router-dom";
+function Log() {
+    const [messages, setMessages] = useState([]);
     const navigate = useNavigate();
 
-    return (
-        <div className="min-h-screen bg-black text-green-500 font-bold flex flex-col px-6">
+    useEffect(() => {
+        const Scrape = async () => {
+            const querySnapshot = await getDocs(collection(db, "User_Data"));
+            const data = querySnapshot.docs.map(doc => ({
+                id: doc.id,
+                ...doc.data()
+            }));
+            setMessages(data);
+        };
 
+        Scrape();
+    }, []);
+
+    return (
+        <div className="min-h-screen bg-black text-green-500 font-bold flex flex-col justify-center items-center px-6">
             <DropdownMenu>
                 <DropdownMenuTrigger className={"absolute top-0 right-2 size-16 right"}>Menu</DropdownMenuTrigger>
                 <DropdownMenuContent>
@@ -26,9 +39,20 @@ function log() {
                     <DropdownMenuItem onClick={() => navigate("/contact")}>Contact</DropdownMenuItem>
                 </DropdownMenuContent>
             </DropdownMenu>
-            <h1 className={"text-7xl flex flex-col  justify-center items-center mt-5"}>Logs:</h1>
-        </div>
-    )
-}
+            <h1 className="text-4xl font-bold mb-4 mt-4">Contact Form Responses:</h1>
 
-export default log
+            <div>
+                {messages.map(msg => (
+                    <div className ={"border-2 border-amber-600 p-5 rounded text-green-500 mt-4 mb-4"}>
+                        <p><strong>Name:</strong> {msg.Name}</p>
+                        <p><strong>Email:</strong> {msg.Email}</p>
+                        <p><strong>Message:</strong> {msg.Msg}</p>
+                        <p>
+                        </p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
+export default Log
